f73a309565d5fa46463520af3a770c4c5fbb74ba
Pvelez476 <pvelez@students.mchenry.edu>
2015-09-07 21:32:23 -0500

    Initial commit

---
+++ variablelibrary/src/main/java/com/example/MyVariable.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class MyVariable {
+    public static void main(String[] args)
+    {
+        //Patrick Velez. Program used to display variables.
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("Boolean");
+        System.out.println("char");
+    }
+
+}
